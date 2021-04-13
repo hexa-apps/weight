7bce9a47b50c13fad328334199d35d7a1069b11e
berkayoruc <berkayoruc@hotmail.com>
2021-04-13 22:35:26 +0300

    chart init

--- lib/pages/graphic_page.dart
+++ lib/pages/graphic_page.dart
@@ -43,16 +43,23 @@ class _GraphicPageState extends State<GraphicPage> {
                     } else {
                       if (snapshot.data.first.length > 0) {
                         var dates = snapshot.data.first;
-                        var weightData = snapshot.data.last;
+                        List<double> weightData = snapshot.data.last
+                            .map<double>((weight) => double.parse(weight))
+                            .toList();
                         return charts.TimeSeriesChart(
                           _createSampleData(dates, weightData),
                           animate: false,
+                          primaryMeasureAxis: charts.NumericAxisSpec(
+                              tickProviderSpec:
+                                 charts.BasicNumericTickProviderSpec(
+                                      zeroBound: false)),
                           defaultRenderer: charts.LineRendererConfig(),
                           customSeriesRenderers: [
                             charts.PointRendererConfig(
                                 customRendererId: 'customPoint')
                           ],
                           dateTimeFactory: const charts.LocalDateTimeFactory(),
+                          behaviors: [charts.PanAndZoomBehavior()],
                         );
                       } else {
                         return Text('Kilo ekle');
@@ -63,51 +70,51 @@ class _GraphicPageState extends State<GraphicPage> {
             )));
   }
 
-  List<charts.Series<TimeSeriesSales, DateTime>> _createSampleData(
+  List<charts.Series<TimeSeriesWeight, DateTime>> _createSampleData(
       dates, weightData) {
-    var desktopSalesData = <TimeSeriesSales>[];
-    var tableSalesData = <TimeSeriesSales>[];
-    var mobileSalesData = <TimeSeriesSales>[];
+    var desktopSalesData = <TimeSeriesWeight>[];
+    // var tableSalesData = <TimeSeriesWeight>[];
+    var mobileSalesData = <TimeSeriesWeight>[];
     for (var i = 0; i < dates.length; i++) {
-      desktopSalesData.add(TimeSeriesSales(
+      desktopSalesData.add(TimeSeriesWeight(
           DateTime(
               int.parse(dates.elementAt(i).split('-').first),
               int.parse(dates.elementAt(i).split('-').elementAt(1)),
               int.parse(dates.elementAt(i).split('-').last)),
-          double.parse(weightData.elementAt(i))));
-      tableSalesData.add(TimeSeriesSales(
+          weightData.elementAt(i)));
+      // tableSalesData.add(TimeSeriesWeight(
+      //     DateTime(
+      //         int.parse(dates.elementAt(i).split('-').first),
+      //         int.parse(dates.elementAt(i).split('-').elementAt(1)),
+      //         int.parse(dates.elementAt(i).split('-').last)),
+      //     weightData.elementAt(i)));
+      mobileSalesData.add(TimeSeriesWeight(
           DateTime(
               int.parse(dates.elementAt(i).split('-').first),
               int.parse(dates.elementAt(i).split('-').elementAt(1)),
               int.parse(dates.elementAt(i).split('-').last)),
-          double.parse(weightData.elementAt(i))));
-      mobileSalesData.add(TimeSeriesSales(
-          DateTime(
-              int.parse(dates.elementAt(i).split('-').first),
-              int.parse(dates.elementAt(i).split('-').elementAt(1)),
-              int.parse(dates.elementAt(i).split('-').last)),
-          double.parse(weightData.elementAt(i))));
+          weightData.elementAt(i)));
     }
     return [
-      charts.Series<TimeSeriesSales, DateTime>(
+      charts.Series<TimeSeriesWeight, DateTime>(
         id: 'Desktop',
         colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
-        domainFn: (TimeSeriesSales sales, _) => sales.time,
-        measureFn: (TimeSeriesSales sales, _) => sales.sales,
+        domainFn: (TimeSeriesWeight sales, _) => sales.time,
+        measureFn: (TimeSeriesWeight sales, _) => sales.sales,
         data: desktopSalesData,
       ),
-      charts.Series<TimeSeriesSales, DateTime>(
-        id: 'Tablet',
-        colorFn: (_, __) => charts.MaterialPalette.red.shadeDefault,
-        domainFn: (TimeSeriesSales sales, _) => sales.time,
-        measureFn: (TimeSeriesSales sales, _) => sales.sales,
-        data: tableSalesData,
-      ),
-      charts.Series<TimeSeriesSales, DateTime>(
+      // charts.Series<TimeSeriesWeight, DateTime>(
+      //   id: 'Tablet',
+      //   colorFn: (_, __) => charts.MaterialPalette.red.shadeDefault,
+      //   domainFn: (TimeSeriesWeight sales, _) => sales.time,
+      //   measureFn: (TimeSeriesWeight sales, _) => sales.sales,
+      //   data: tableSalesData,
+      // ),
+      charts.Series<TimeSeriesWeight, DateTime>(
           id: 'Mobile',
           colorFn: (_, __) => charts.MaterialPalette.green.shadeDefault,
-          domainFn: (TimeSeriesSales sales, _) => sales.time,
-          measureFn: (TimeSeriesSales sales, _) => sales.sales,
+          domainFn: (TimeSeriesWeight sales, _) => sales.time,
+          measureFn: (TimeSeriesWeight sales, _) => sales.sales,
           data: mobileSalesData)
         // Configure our custom point renderer for this series.
         ..setAttribute(charts.rendererIdKey, 'customPoint'),
@@ -115,9 +122,9 @@ class _GraphicPageState extends State<GraphicPage> {
   }
 }
 
-class TimeSeriesSales {
+class TimeSeriesWeight {
   final DateTime time;
   final double sales;
 
-  TimeSeriesSales(this.time, this.sales);
+  TimeSeriesWeight(this.time, this.sales);
 }
